887df0c63f0f0000ad2da85c724f142b402d072a
unknown <vip.vova556@gmail.com>
2021-12-11 20:14:23 +0200

    first commit

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -6,11 +6,15 @@
       dark >
       <v-toolbar-title>Table Test</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
+      <v-btn
+        icon
+        :disabled="true">
         <v-icon>mdi-heart</v-icon>
       </v-btn>
       <Search />
-      <v-btn icon>
+      <v-btn
+        icon
+        :disabled="true">
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
